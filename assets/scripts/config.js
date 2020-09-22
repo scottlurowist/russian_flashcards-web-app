0b00854d0f8ac450842cce732f6c458193cd148c
scottlurowist <scottlurowist@gmail.com>
2020-09-22 15:26:28 -0400

    LET ME TRY THIS AGAIN WITH THE URL
    The deployed web app is not communicating with the backend on Heroku.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://russian-flashcards-webapi.herokuapp.com',
+  production: ' https://russian-flashcards-webapi.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
